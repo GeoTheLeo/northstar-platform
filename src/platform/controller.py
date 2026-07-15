592259a5e48826a7f08a023ad63845a4d950ce99
GeoTheLeo <professorgeotheleo@outlook.com>
2026-07-15 17:59:57 +0200

    Introduce platform controller

---
+++ src/platform/controller.py
@@ -0,0 +1,22 @@
+"""
+NorthStar Platform Controller
+
+Coordinates platform services and exposes
+application-ready data to the UI layer.
+"""
+
+from platform.services.dashboard_service import DashboardService
+
+
+class NorthStarController:
+    """
+    Application orchestration layer.
+    """
+
+    def __init__(self):
+
+        self.dashboard_service = DashboardService()
+
+    def dashboard(self):
+
+        return self.dashboard_service.load_dashboard()
